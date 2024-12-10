f2aaed3658892e2b9f3ff8d83139478bf782c212
paudelb36 <paudelb11@gmail.com>
2024-12-10 23:28:35 +0545

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Include all relevant files
   ],
